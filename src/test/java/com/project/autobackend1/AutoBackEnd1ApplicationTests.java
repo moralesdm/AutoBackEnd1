596cfba4edb371bdc030933d823a0efb74893c9a
moralesdm <morales.david.gr.22@gmail.com>
2025-06-16 22:46:17 -0500

    second commit

---
+++ src/test/java/com/project/autobackend1/AutoBackEnd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.autobackend1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBackEnd1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
