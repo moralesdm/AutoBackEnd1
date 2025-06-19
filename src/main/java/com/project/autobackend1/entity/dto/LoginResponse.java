82676440c92c19e413263081c4750a0c7f7c0b57
moralesdm <morales.david.gr.22@gmail.com>
2025-06-18 21:27:03 -0500

    feat(dto): create LoginResponse DTO to return JWT token to client

---
+++ src/main/java/com/project/autobackend1/entity/dto/LoginResponse.java
@@ -0,0 +1,12 @@
+package com.project.autobackend1.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+}
+
+
