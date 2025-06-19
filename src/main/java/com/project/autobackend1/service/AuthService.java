e425b27e8bc73751e40a82315a1b68cc88e1417d
moralesdm <morales.david.gr.22@gmail.com>
2025-06-18 21:58:09 -0500

    feat(service): add register method to AuthService interface

--- src/main/java/com/project/autobackend1/service/AuthService.java
+++ src/main/java/com/project/autobackend1/service/AuthService.java
@@ -2,7 +2,9 @@ package com.project.autobackend1.service;
 
 import com.project.autobackend1.entity.dto.LoginRequest;
 import com.project.autobackend1.entity.dto.LoginResponse;
+import com.project.autobackend1.entity.dto.RegisterRequest;
 
 public interface AuthService {
     LoginResponse login(LoginRequest request);
+    LoginResponse register(RegisterRequest request);
 }
