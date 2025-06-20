6e4a7b85002c1532284111fcdb685e560753ad78
moralesdm <morales.david.gr.22@gmail.com>
2025-06-19 21:32:35 -0500

    feat(repository): create PasswordResetTokenRepository to manage recovery tokens

---
+++ src/main/java/com/project/autobackend1/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,10 @@
+package com.project.autobackend1.repository;
+
+import com.project.autobackend1.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String token);
+}
